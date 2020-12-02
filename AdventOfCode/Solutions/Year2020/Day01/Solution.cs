8126cc9755a8a991988ef85b4f28e01f83a1fc39
Petar Kostic <snookik@gmail.com>
2020-12-02 02:11:31 +0100

    Added solutions for Day 1: Report Repair

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,26 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day01 : ASolution
     {
+        private readonly int[] parsedInput;
 
-        public Day01() : base(01, 2020, "")
+        public Day01() : base(01, 2020, "Report Repair")
         {
+            string[] lines = Input.SplitByNewline();
 
+            parsedInput = new int[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+                parsedInput[i] = int.Parse(lines[i]);
         }
 
+        /// <summary>
+        /// First naive solution O(N^2)
+        /// </summary>
         protected override string SolvePartOne()
         {
-            return null;
+            int part1Result = 0;
+
+            for (int i = 0; i < parsedInput.Length - 1; i++)
+                for (int j = i + 1; j < parsedInput.Length; j++)
+                {
+                    if (parsedInput[i] + parsedInput[j] == 2020)
+                        part1Result = parsedInput[i] * parsedInput[j];
+                }
+            return part1Result.ToString();
         }
 
+        /// <summary>
+        /// First naive solution O(N^3)
+        /// </summary>
         protected override string SolvePartTwo()
         {
-            return null;
+            int part2Result = 0;
+
+            for (int i = 0; i < parsedInput.Length - 2; i++)
+                for (int j = i + 1; j < parsedInput.Length - 1; j++)
+                    for (int z = j + 1; z < parsedInput.Length; z++)
+                    {
+                        if (parsedInput[i] + parsedInput[j] + parsedInput[z] == 2020)
+                            part2Result = parsedInput[i] * parsedInput[j] * parsedInput[z];
+                    }
+                   
+            return part2Result.ToString();
         }
     }
 }
