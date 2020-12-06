3854f7b22f3391d9bd31c12dc9cf6b530c8fedc2
Petar Kostic <pkositc@acto.nl>
2020-12-06 21:14:22 +0100

    Added solutions for Day 6: Custom Customs

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -1,26 +1,35 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day06 : ASolution
+	class Day06 : ASolution
     {
+	    private readonly string[][] _parsedInput;
 
-        public Day06() : base(06, 2020, "")
+        public Day06() : base(06, 2020, "Custom Customs")
         {
-
+	        _parsedInput = Input.Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries)
+									  .Select(x => x.Split('\n'))
+									  .ToArray();
         }
 
+        /// <summary>
+        /// For each group, count the number of questions to which anyone answered "yes"
+        /// flatten jagged array from input and count the distincts, sum those
+        /// </summary>
         protected override string SolvePartOne()
         {
-            return null;
+	        return _parsedInput.Sum(x => x.SelectMany(y => y).Distinct().Count()).ToString();
         }
 
+        /// <summary>
+        /// For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+        /// Count the number of elements for all that occur multiple times in the group, sum those
+        /// </summary>
         protected override string SolvePartTwo()
         {
-            return null;
+	        return _parsedInput.Sum(x => x.First().Count(y => x.All(z => z.Contains(y)))).ToString();
         }
     }
 }
