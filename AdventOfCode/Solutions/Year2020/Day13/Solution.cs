8bf921d96f760f2f5d7a441bc8b97f25b263cba7
Petar Kostic <snookik@gmail.com>
2020-12-13 15:34:39 +0100

    Added solutions for Day 13: Shuttle Search

--- AdventOfCode/Solutions/Year2020/Day13/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day13/Solution.cs
@@ -1,26 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day13 : ASolution
+    internal class Day13 : ASolution
     {
+        private readonly int _earliestDepart;
+        private readonly int[] _busDepartures;
 
-        public Day13() : base(13, 2020, "")
+        public Day13() : base(13, 2020, "Shuttle Search")
         {
-
+            var parsedInput = Input.SplitByNewline();
+            _earliestDepart = int.Parse(parsedInput[0]);
+            _busDepartures = parsedInput[1].Split(',')
+                                           .Select(x => int.TryParse(x, out var parsedDepart) ? parsedDepart : 0)
+                                           .ToArray();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var (time, busId) = _busDepartures.Where(bus => bus > 0)
+                                                     .Select(bus => ((_earliestDepart / bus + 1) * bus, bus))
+                                                     .Min();
+            return ((time - _earliestDepart) * busId).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            long earliest = _busDepartures[0];
+            long inc = earliest;
+
+            for (int i = 1; i < _busDepartures.Length; i++)
+            {
+                if (_busDepartures[i] == 0) // Don't do .Where(x => x > 0) initially like Part 1, since i++ will be skipped for mod iterations
+                    continue;
+                do earliest += inc; 
+                while (earliest % _busDepartures[i] != _busDepartures[i] - i % _busDepartures[i]);
+
+                inc = (long)Utilities.FindLCM(inc, _busDepartures[i]);
+            }
+            return earliest.ToString();
         }
     }
 }
