c84d27e0dc0f493cc63c69dbdca174d91f3c2512
Petar Kostic <snookik@gmail.com>
2020-12-15 10:58:47 +0100

    Added solutions for Day 15: Rambunctious Recitation

--- AdventOfCode/Solutions/Year2020/Day15/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Solution.cs
@@ -1,26 +1,36 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
-    class Day15 : ASolution
+    internal class Day15 : ASolution
     {
+        private readonly int[] _parsedInput;
 
-        public Day15() : base(15, 2020, "")
+        public Day15() : base(15, 2020, "Rambunctious Recitation")
         {
-
+            _parsedInput = Input.Split(',')
+                                .Select(int.Parse)
+                                .ToArray();
         }
 
-        protected override string SolvePartOne()
-        {
-            return null;
-        }
+        protected override string SolvePartOne() => SolveGame(2020).ToString();
+        protected override string SolvePartTwo() => SolveGame(30000000).ToString();
 
-        protected override string SolvePartTwo()
+        private int SolveGame(int target)
         {
-            return null;
+            Dictionary<int, (int num, int index)> spoken = _parsedInput.Select((number, index) => (number, index))
+                                                                       .ToDictionary(x => x.number, x => (-1, x.index));
+            var last = spoken.Last().Key;
+
+            for (var i = _parsedInput.Length; i < target; i++)
+            {
+                if (spoken.TryGetValue(last, out var lastIndex))
+                    last = lastIndex.num != -1 ? lastIndex.index - lastIndex.num : 0;
+
+                spoken[last] = spoken.ContainsKey(last) ? (spoken[last].index, i) : (-1, i);
+            }
+            return last;
         }
     }
 }
