d84cee00bd486c015f91f051378e9457d05902c1
Petar Kostic <snookik@gmail.com>
2020-12-17 22:48:25 +0100

    Added Enumerable.ForEach extension method, small cleanup in Utilities.cs

--- AdventOfCode/Solutions/Utilities.cs
+++ AdventOfCode/Solutions/Utilities.cs
@@ -126,17 +126,22 @@ namespace AdventOfCode.Solutions
         // https://stackoverflow.com/questions/49190830/is-it-possible-for-string-split-to-return-tuple
         public static void Deconstruct<T>(this IList<T> list, out T first, out IList<T> rest)
         {
-            first = list.Count > 0 ? list[0] : default(T); // or throw
+            first = list.Count > 0 ? list[0] : default; // or throw
             rest = list.Skip(1).ToList();
         }
 
         public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out IList<T> rest)
         {
-            first = list.Count > 0 ? list[0] : default(T); // or throw
-            second = list.Count > 1 ? list[1] : default(T); // or throw
+            first = list.Count > 0 ? list[0] : default; // or throw
+            second = list.Count > 1 ? list[1] : default; // or throw
             rest = list.Skip(2).ToList();
         }
 
         public static (int, int) Add(this (int x, int y) a, (int x, int y) b) => (a.x + b.x, a.y + b.y);
+
+        public static void ForEach<T>(this IEnumerable<T> @enum, Action<T> mapFunction)
+        {
+            foreach (var item in @enum) mapFunction(item);
+        }
     }
 }
