29de74c7972db06575b6bf59e9ab05a8c6c67690
Petar Kostić <petar@kostic.dev>
2021-12-05 17:24:56 +0100

    Added solutions for Day 1: Sonar Sweep

--- AdventOfCode/Solutions/Year2021/Day01/Day01.cs
+++ AdventOfCode/Solutions/Year2021/Day01/Day01.cs
@@ -7,20 +7,45 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day01 : ASolution
     {
+        private readonly int[] parsedInput;
 
-        public Day01() : base(01, 2021, "")
+        public Day01() : base(01, 2021, "Sonar Sweep")
         {
-
+            string[] lines = Input.SplitByNewline();
+            parsedInput = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                parsedInput[i] = int.Parse(lines[i]);
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int amount = 0;
+            for (int i = 1; i < parsedInput.Length; i++)
+            {
+                if (parsedInput[i] > parsedInput[i - 1])
+                    amount++;
+            }
+            return amount.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int amount = 0;
+
+            for (int i = 1; i < parsedInput.Length - 2; i++)
+            {
+                int first = parsedInput[i - 1] + parsedInput[i] + parsedInput[i + 1];
+                int second = parsedInput[i] + parsedInput[i + 1] + parsedInput[i + 2];
+
+                if (second > first)
+                {
+                    amount++;
+                }
+            }
+
+            return amount.ToString();
         }
     }
 }
