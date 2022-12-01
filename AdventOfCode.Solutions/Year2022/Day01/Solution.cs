df82a75d10e5fc6884c97ee4c6f9d209768c4eac
Petar Kostic <petar@kostic.dev>
2022-12-01 12:18:16 +0100

    Added solutions for Day 1: Calorie Counting

--- AdventOfCode.Solutions/Year2022/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day01/Solution.cs
@@ -2,15 +2,38 @@ namespace AdventOfCode.Solutions.Year2022.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2022, "") { }
+    private readonly Dictionary<int, int> calorieDic;
+
+    public Solution() : base(01, 2022, "Calorie Counting") 
+    {
+        this.calorieDic = new Dictionary<int, int>();
+
+        string[] splitByParagraphInput = Input.SplitByParagraph(true);
+
+        for (int i = 0; i < splitByParagraphInput.Length; i++)
+        {
+            string? calorieLines = splitByParagraphInput[i];
+            string[] caloriesForElf = calorieLines.SplitByNewline();
+
+            foreach (var calorieForElf in caloriesForElf)
+            {
+                int calorieAsInt = int.Parse(calorieForElf);
+
+                if (!calorieDic.ContainsKey(i))
+                    this.calorieDic.Add(i, calorieAsInt);
+                else 
+                    this.calorieDic[i] += calorieAsInt;
+            }
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return calorieDic.Values.Max().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return calorieDic.OrderByDescending(x => x.Value).Select(x => x.Value).Take(3).Sum().ToString();
     }
 }
