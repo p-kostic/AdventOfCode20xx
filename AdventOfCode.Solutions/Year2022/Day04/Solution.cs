9a1a5635635e9ff359fc0caca4865ad3b2de040b
Petar Kostić <petar@kostic.dev>
2022-12-04 11:10:59 +0100

    Added solutions for Day 4: Camp Cleanup

--- AdventOfCode.Solutions/Year2022/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day04/Solution.cs
@@ -1,16 +1,39 @@
 namespace AdventOfCode.Solutions.Year2022.Day04;
 
-class Solution : SolutionBase
+internal class Solution : SolutionBase
 {
-    public Solution() : base(04, 2022, "") { }
+    private readonly List<int[][]> _parsedInput;
+
+    public Solution() : base(04, 2022, "Camp Cleanup")
+    {
+        _parsedInput = Input.SplitByNewline()
+                            .Select(x => x.Split(",")
+                                                .Select(y => y.Split("-")
+                                                                    .Select(int.Parse)
+                                                                    .ToArray()
+                                                       )
+                                                .ToArray()
+                                   )
+                            .ToList();
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var count = 0;
+        foreach (var pairs in _parsedInput)
+            if ((pairs[0][0] >= pairs[1][0] && pairs[0][1] <= pairs[1][1]) || (pairs[0][0] <= pairs[1][0] && pairs[0][1] >= pairs[1][1])) 
+                count++;
+
+        return count.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var count = 0;
+        foreach (var pairs in _parsedInput)
+            if (pairs[0][0] <= pairs[1][1] && pairs[0][1] >= pairs[1][0]) 
+                count++;
+        
+        return count.ToString();
     }
 }
