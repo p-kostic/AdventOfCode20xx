aa2a6894ee1c459b44f1d37713bbe7e850aec0af
Petar Kostic <petar@kostic.dev>
2022-12-06 10:04:46 +0100

    Added solutions for Day 6: Tuning Trouble

--- AdventOfCode.Solutions/Year2022/Day06/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day06/Solution.cs
@@ -2,15 +2,23 @@ namespace AdventOfCode.Solutions.Year2022.Day06;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(06, 2022, "") { }
+    public Solution() : base(06, 2022, "Tuning Trouble") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return SubsequentDistinct(4);
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return SubsequentDistinct(14);
+    }
+
+    private string SubsequentDistinct(int length)
+    {
+        for (int i = 0; i < this.Input.Length; i++)
+            if (this.Input.Skip(i).Take(length).Distinct().Count() == length)
+                return (i + length).ToString();
+        return "no answer";
     }
 }
