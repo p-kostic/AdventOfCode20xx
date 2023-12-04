b0c0b9f1422f0e76d22985d3552b6142e8af65f0
Petar Kostić <petar.kostic@dimaros.nl>
2023-12-04 14:15:26 +0100

    Initialize Day 5

---
+++ AdventOfCode.Solutions/Year2023/Day05/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.Solutions.Year2023.Day05;
+
+internal class Solution : SolutionBase
+{
+    
+    public Solution() : base(05, 2023, "")
+    {
+        
+    }
+    
+    protected override string SolvePartOne()
+    {
+        return string.Empty;
+    }
+ 
+    protected override string SolvePartTwo()
+    {
+        return string.Empty;
+    }
+}
