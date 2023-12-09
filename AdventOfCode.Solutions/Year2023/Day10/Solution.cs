76dd06990087f30563ea18b1698beb01de72d08e
Petar Kostić <snookik@gmail.com>
2023-12-09 11:51:24 +0100

    Initialize Day 10

---
+++ AdventOfCode.Solutions/Year2023/Day10/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Solutions.Year2023.Day10;
+
+internal class Solution : SolutionBase
+{
+    public Solution(): base(10, 2023, "") { }
+
+    protected override string SolvePartOne() => string.Empty;
+    protected override string SolvePartTwo() => string.Empty;
+}
