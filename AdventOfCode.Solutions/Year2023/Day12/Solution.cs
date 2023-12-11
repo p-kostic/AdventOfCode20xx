0ca8f051ffad53c804cdf2910b372dc8ce165b3a
Petar Kostic <petar@kostic.dev>
2023-12-11 23:47:53 +0100

    Initialize Day 12, rename folder of Day 11

---
+++ AdventOfCode.Solutions/Year2023/Day12/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Solutions.Year2023.Day12;
+
+internal class Solution : SolutionBase
+{
+    public Solution() : base(12, 2023, "") { }
+
+    protected override string SolvePartOne() => string.Empty;
+    protected override string SolvePartTwo() => string.Empty;
+}
