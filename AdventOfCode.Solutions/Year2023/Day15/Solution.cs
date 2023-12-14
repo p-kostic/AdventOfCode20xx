df1b94c1b1966524b7c1b29d01d82234dcdefc99
Petar Kostic <petar@kostic.dev>
2023-12-15 00:19:19 +0100

    Initialize Day 15

---
+++ AdventOfCode.Solutions/Year2023/Day15/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode.Solutions.Year2023.Day15;
+
+internal class Solution : SolutionBase
+{
+    public Solution() : base(15, 2023, "Parabolic Reflector Dish") { }
+
+    protected override string SolvePartOne()
+    {
+        return string.Empty;
+    }
+
+    protected override string SolvePartTwo()
+    {
+        return string.Empty;
+    }
+}
