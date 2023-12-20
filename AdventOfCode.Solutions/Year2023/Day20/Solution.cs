9538c314a2f36f7004f823e0e72874c9aab58c8e
Petar Kostic <petar@kostic.dev>
2023-12-20 01:16:17 +0100

    Initialize Day 20

---
+++ AdventOfCode.Solutions/Year2023/Day20/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Solutions.Year2023.Day20;
+
+internal partial class Solution : SolutionBase
+{
+    public Solution() : base(20, 2023, "") { }
+
+    protected override string SolvePartOne()
+    {
+        return string.Empty;
+    }
+    protected override string SolvePartTwo()
+    {
+        return string.Empty;
+    }
+}
